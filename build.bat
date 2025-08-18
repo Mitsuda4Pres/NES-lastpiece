49b73a69a17a72fad2131182c73a88c6e0767d90
Mitsuda4Pres <duncan.stgelais@gmail.com>
2025-08-18 14:41:06 -0400

    Famistudio engine included in git folder.

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 ca65 main.asm
-ld65 main.o -o main.nes -t nes
+ca65 pently\pentlysound.s
+ca65 pently\pentlymusic.s
+ca65 pently\musicseq.s
+ca65 pently\ntscPeriods.s
+ld65 main.o pentlysound.o pentlymusic.o musicseq.o ntscPeriod.o -o main.nes -t nes
 .\main.nes
